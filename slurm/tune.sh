3d4221ff77d89e8103cf14a44bf4c6041bf88e33
Matteo <spartacus990@gmail.com>
2025-08-03 12:04:40 +0200

    🧑‍💻 Add new finetune code
    Add new code to finetune the model

---
+++ slurm/tune.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#SBATCH --job-name musicgen-medium
+#SBATCH --output log/out/%j.txt
+#SBATCH --error log/err/%j.txt
+#SBATCH --mail-type ALL
+#SBATCH --time 10-00:00:00
+#SBATCH --ntasks-per-node 1
+#SBATCH --partition allgroups
+#SBATCH --mem 35G
+#SBATCH --gres=gpu:a40:4
+
+source .env
+
+export HYDRA_FULL_ERROR=1
+export CUDA_LAUNCH_BLOCKING=1
+export TORCH_DISTRIBUTED_DEBUG=ERROR
+export PATH="$PATH:$HOME/ffmpeg:$HOME/.local/bin"
+
+export DORA_PACKAGE="audiocraft"
+export AUDIOCRAFT_DORA_DIR="$OUTPATH/dora"
+export AUDIOCRAFT_REFERENCE_DIR="$OUTPATH/reference"
+
+srun --mail-user "$EMAIL" ~/miniconda3/bin/conda run -n musicgen \
+	dora run -d \
+		solver=musicgen/musicgen_base_32khz \
+		model/lm/model_scale=medium \
+		continue_from=//pretrained/facebook/musicgen-medium \
+		conditioner=text2music \
+		dset=audio/train \
+		logging.log_wandb=true \
+		wandb.project=musicgen-medium \
+		dataset.num_workers=1 \
+		dataset.valid.num_samples=1 \
+		dataset.batch_size=4 \
+		schedule.cosine.warmup=8 \
+		generate.lm.prompted_samples=False \
+		generate.lm.gen_gt_samples=True \
+		autocast=false \
+        deadlock.use=false \
+		fsdp.use=true \
+        fsdp.sharding_strategy=full_shard \
+		optim.lr=1e-4 \
+		optim.epochs=50 \
+		optim.updates_per_epoch=2000 \
+		optim.optimizer=adamw
