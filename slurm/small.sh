8ece4a6b16fe7355fa1be79a57cdb764e13d7703
Spanio Matteo <spanio@login.dei.unipd.it>
2025-08-10 15:15:56 +0200

    Update SLURM scripts
    Add small training config and update medium and large batch jobs

--- slurm/small.sh
+++ slurm/small.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name musicgen-large
+#SBATCH --job-name musicgen-small
 #SBATCH --output log/out/%j.txt
 #SBATCH --error log/err/%j.txt
 #SBATCH --mail-type ALL
-#SBATCH --time 30-00:00:00
+#SBATCH --time 10-00:00:00
 #SBATCH --partition allgroups
-#SBATCH --nodes 2
+#SBATCH --nodes 1
 #SBATCH --cpus-per-task 8
-#SBATCH --gres gpu:a40:4
-#SBATCH --mem 64G
+#SBATCH --gres gpu:a40:2
+#SBATCH --mem 32G
 
 source .env
 
@@ -22,22 +22,23 @@ export AUDIOCRAFT_REFERENCE_DIR="$OUTPATH/reference"
 srun --mail-user "$EMAIL" ~/miniconda3/bin/conda run -n musicgen \
 	dora run -d \
 		solver=musicgen/musicgen_base_32khz \
-		model/lm/model_scale=large \
-		continue_from=//pretrained/facebook/musicgen-large \
+		model/lm/model_scale=small \
+		continue_from=//pretrained/facebook/musicgen-small \
 		conditioner=text2music \
 		dset=audio/tasty_music \
 		logging.log_wandb=true \
-		wandb.project=musicgen-large \
-		dataset.num_workers=4 \
-		dataset.batch_size=2 \
+		wandb.project=musicgen-small \
+		dataset.num_workers=8 \
+		dataset.batch_size=32 \
 		schedule.cosine.warmup=8 \
 		generate.lm.prompted_samples=false \
 		generate.lm.gen_gt_samples=true \
 		autocast=false \
 		fsdp.use=true \
-        checkpoint.save_every=2 \
-        checkpoint.keep_last=5 \
+		schedule.cosine.warmup=8 \
+		checkpoint.save_every=2 \
+		checkpoint.keep_last=5 \
 		optim.lr=1e-4 \
-		optim.epochs=50 \
+		optim.epochs=100 \
 		optim.updates_per_epoch=2000 \
 		optim.optimizer=adamw
--- slurm/tune.sh
+++ slurm/tune.sh
@@ -4,20 +4,19 @@
 #SBATCH --error log/err/%j.txt
 #SBATCH --mail-type ALL
 #SBATCH --time 10-00:00:00
-#SBATCH --ntasks-per-node 1
 #SBATCH --partition allgroups
-#SBATCH --mem 35G
-#SBATCH --gres=gpu:a40:4
+#SBATCH --nodes 1
+#SBATCH --cpus-per-task 8
+#SBATCH --gres gpu:a40:4
+#SBATCH --mem 64G
 
 source .env
 
-export HYDRA_FULL_ERROR=1
-export CUDA_LAUNCH_BLOCKING=1
-export TORCH_DISTRIBUTED_DEBUG=OFF # change to INFO for more verbose output
-export PATH="$PATH:$HOME/ffmpeg:$HOME/.local/bin"
+spack load ffmpeg
 
 export DORA_PACKAGE="audiocraft"
 export AUDIOCRAFT_DORA_DIR="$OUTPATH/dora"
+export AUDIOCRAFT_CACHE_DIR="$OUTPATH/cache"
 export AUDIOCRAFT_REFERENCE_DIR="$OUTPATH/reference"
 
 srun --mail-user "$EMAIL" ~/miniconda3/bin/conda run -n musicgen \
@@ -29,18 +28,17 @@ srun --mail-user "$EMAIL" ~/miniconda3/bin/conda run -n musicgen \
 		dset=audio/tasty_music \
 		logging.log_wandb=true \
 		wandb.project=musicgen-medium \
-		dataset.num_workers=1 \
-		dataset.valid.num_samples=1 \
-		dataset.batch_size=4 \
+		dataset.num_workers=8 \
+		dataset.batch_size=16 \
 		schedule.cosine.warmup=8 \
-		generate.lm.prompted_samples=False \
-		generate.lm.gen_gt_samples=True \
+		generate.lm.prompted_samples=false \
+		generate.lm.gen_gt_samples=true \
 		autocast=false \
-		deadlock.use=false \
 		fsdp.use=true \
-        checkpoint.save_every=2 \
-        checkpoint.keep_last=5 \
+		schedule.cosine.warmup=8 \
+		checkpoint.save_every=2 \
+		checkpoint.keep_last=5 \
 		optim.lr=1e-4 \
-		optim.epochs=50 \
+		optim.epochs=100 \
 		optim.updates_per_epoch=2000 \
 		optim.optimizer=adamw
